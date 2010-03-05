1453b8b295417f06c19613e8e9562b30948398b4
Tim Oram <mitmaro@mitmaro.ca>
2010-03-05 00:20:07 -0330

    Added the config template file

---
+++ php/config.template.php
@@ -0,0 +1,20 @@
+<?php
+/*------------------------------------------------------------------------------
+    File: php/config.template.php
+ Project: PHP Google Latitude Updater
+ Version: 0.1.0
+      By: Tim Oram <t.oram@mitmaro.ca>
+ License: MIT License. See COPYING for full license
+Instructions
+   Make a copy of this file and call it config.php. Set both username and
+   password to your google account login information and choose a passkey.
+------------------------------------------------------------------------------*/
+
+// your google account information
+$username = '';
+$password = '';
+
+// a passkey, make it something other than your google password as this will be
+// send plain text from your cellphone
+$passkey = '';
+
